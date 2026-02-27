609ec9f93a63a90a64c18b0874308770b7ab7abb
bs.koo <bonseung@sqisoft.com>
2026-02-27 16:55:06 +0900

    Initialize Spring Boot project with Gradle build, application configuration, and project documentation

---
+++ src/main/java/com/oda/OdaApplication.java
@@ -0,0 +1,13 @@
+package com.oda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oda/OdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
